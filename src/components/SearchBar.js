c07b25e299d4f0be72b22132a4d830942d070be1
pauldidonatojr <pjdidonatojr@gmail.com>
2022-07-10 16:07:58 -0400

    push

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -56,7 +56,7 @@ const Wrapper = styled.div`
     background: transparent;
     border-color: transparent;
     text-transform: capitalize;
-    font-size: 0.85rem;
+    font-size: 0.55rem;
     letter-spacing: var(--spacing);
     margin: 0 0.5rem;
     transition: var(--transition);
@@ -100,7 +100,7 @@ const Wrapper = styled.div`
    border: none;
    text-transform: capitalize;
    letter-spacing: var(--spacing);
-   font-size: 1.5rem;
+   font-size: 0.5rem;
    color: black;
    border-bottom: 3px solid var(--clr-grey-5);
   }
@@ -123,76 +123,6 @@ const Wrapper = styled.div`
  }
 
  @media screen and (max-width: 992px) {
-  /* display: grid;
-  .div-3 {
-   display: none;
-  }
- }
- .div-1 {
-  flex: 1;
-  ul {
-   display: flex;
-   justify-content: 'center';
-   li {
-    color: white;
-    margin-right: 0.5rem;
-    margin-left: 0.5rem;
-    background: transparent;
-    border-color: transparent;
-    text-transform: capitalize;
-    font-size: 0.85rem;
-    letter-spacing: var(--spacing);
-    margin: 0 0.5rem;
-    transition: var(--transition);
-    cursor: pointer;
-    padding: 0.25rem 0;
-    line-height: 1;
-    outline-color: var(--clr-primary-10);
-   }
-   li:hover {
-    color: white;
-    box-shadow: 0 2px var(--clr-primary-10);
-   }
-   .li4 {
-    display: none;
-   }
-  }
- }
- .div-2 {
-  flex: 1;
-  display: flex;
-  h3 {
-   font-size: 2rem;
-  }
-  h3:hover {
-   color: black;
-   box-shadow: 0 4px var(--clr-primary-10);
-  }
-  p {
-   color: black;
-  }
- }
- .div-3 {
-  .search-form {
-   display: flex;
-   justify-content: center;
-  }
 
-  .form-input,
-  .submit-btn {
-   padding: 0.03rem 0.4rem;
-   border: none;
-   text-transform: capitalize;
-   letter-spacing: var(--spacing);
-   font-size: 1.5rem;
-   color: black;
-   border-bottom: 3px solid var(--clr-grey-5);
-  }
-  .form-input {
-   color: var(--clr-grey-3);
-  }
-  .form-input::placeholder {
-   color: var(--clr-grey-5);
-  } */
  }
 `
